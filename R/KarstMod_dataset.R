db57d19ff9943c01665e36692af4940aa6f02611
busemorose <guillaume.cinkus@gmail.com>
2022-07-01 10:52:49 +0200

    init

---
+++ R/KarstMod_dataset.R
@@ -0,0 +1,14 @@
+#' A KarstMod dataset.
+#'
+#' @format A data frame with 365 rows and 7 variables:
+#' \describe{
+#'   \item{date}{date}
+#'   \item{comment}{row number}
+#'   \item{P}{precipitation}
+#'   \item{T}{temperature}
+#'   \item{RH}{relative humidity}
+#'   \item{ET}{evapotranspiration}
+#'   \item{QobsS}{observed discharge}
+#'   ...
+#' }
+"KarstMod_dataset"
