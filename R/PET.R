3c646488fbfa6c8b71554a4c51a41d2cb0f3a456
busemorose <guillaume.cinkus@gmail.com>
2022-07-02 15:05:27 +0200

    Add tornthwaite-pereira daily PET method
    Correct latitude input not needed for Haude method

--- R/PET.R
+++ R/PET.R
@@ -12,6 +12,7 @@
 #' @param k1 A numeric value. Used for Oudin's method.
 #' @param k2 A numeric value. Used for Oudin's method.
 #' @param f_haude A numeric vector of one factor for each month (12 in total). Used for Haude's method.
+#' @param k
 #'
 #' @return A numeric vector.
 #' @export
@@ -20,11 +21,11 @@
 #' data(KarstMod_dataset)
 #' PET(date = KarstMod_dataset$date, t = KarstMod_dataset$T, latitude = 48)
 
-PET <- function(date, t, method = c("oudin2005", "hargreaves1985", "turc1961", "haude1954"), tmax = NULL, tmin = NULL, rh = NULL, latitude = NULL, krs = 0.17, Rs = NULL, k1 = 100, k2 = 5, f_haude = c(0.26, 0.26, 0.33, 0.39, 0.39, 0.37, 0.35, 0.33, 0.31, 0.26, 0.26, 0.26)) {
+PET <- function(date, t, method = c("oudin2005", "hargreaves1985", "turc1961", "haude1954"), tmax = NULL, tmin = NULL, rh = NULL, latitude = NULL, krs = 0.17, Rs = NULL, k1 = 100, k2 = 5, f_haude = c(0.26, 0.26, 0.33, 0.39, 0.39, 0.37, 0.35, 0.33, 0.31, 0.26, 0.26, 0.26), k = 0.69) {
 
   # Calculate extraterrestrial radiation
 
-  if (is.null(latitude))
+  if (is.null(latitude) & !(method %in% c("haude1954")))
     stop("Latitude must be specified.")
 
   latrad <- (latitude * pi) / 180 # conversion in rad
@@ -43,6 +44,7 @@ PET <- function(date, t, method = c("oudin2005", "hargreaves1985", "turc1961", "
   if (method == "oudin2005") {
     if (!all(lengths(list(t)) == length(date)))
       stop("date and t must be specified and with the same length.")
+
     le <- 2.501 - 0.002361 * t # latent heat of vaporization in MJ.kg-1
     oudin <- ifelse((t + 5) > 0, (Ra / le) * ((t + k2) / k1), 0)
     return(oudin)
@@ -51,6 +53,7 @@ PET <- function(date, t, method = c("oudin2005", "hargreaves1985", "turc1961", "
   if (method == "hargreaves1985") {
     if (!all(lengths(list(t, tmin, tmax)) == length(date)))
       stop("date, t, tmin and tmax must be specified and with the same length.")
+
     hargreaves <- 0.0135 * krs * Ra * 0.408 * sqrt(mean(tmax) - mean(tmin)) * (t + 17.8)
     return(hargreaves)
   }
@@ -65,6 +68,7 @@ PET <- function(date, t, method = c("oudin2005", "hargreaves1985", "turc1961", "
       Rs <- (krs * sqrt(tmax - tmin)) * Ra # estimation of incoming solar radiation Rs
       message("Rs was estimated using the FAO guide for PET")
     }
+
     turc <- ifelse(rh >= 50,
                    0.013 * (t / (t + 15)) * (Rs * 23.9 + 50),
                    0.013 * (t / (t + 15)) * (Rs * 23.9 + 50) * (1 + (50 - rh) / 70))
@@ -81,6 +85,27 @@ PET <- function(date, t, method = c("oudin2005", "hargreaves1985", "turc1961", "
                     f = f_haude[month],
                     PET = f * 6.11 * 10 ^ ((7.48 * t) / (237 + t)) * (1 - rh / 100))
     return(haude$PET)
+  }
+
+  if (method == "tornthwaite_pereira2004") {
+    if (!all(lengths(list(tmin, tmax, t)) == length(date)))
+      stop("date, t, and rh must be specified and with the same length.")
+
+    tornthwaite <- data.frame(date, t, tmin, tmax) |>
+      dplyr::group_by(month = lubridate::month(date)) |>
+      dplyr::summarise(tk = mean(t)) |>
+      dplyr::mutate(i = dplyr::case_when(tk > 0 ~ (tk / 5) ^ 1.51,
+                                         TRUE ~ 0))
+
+    I <- sum(tornthwaite$i)
+    a <- (67.5e-8 * I ^ 3) - (77.1e-6 * I ^ 2) + (0.0179 * I) + 0.492
+    kd <- N / 360
+    tef <- 0.5 * k * (3 * tmax - tmin)
+    tornthwaite_pereira <- ifelse(tef > 26,
+                                  kd * (-415.85 + 32.24 * tef - 0.43 * tef ^2),
+                                  ifelse(tef > 0,
+                                         16 * ((10 * tef) / I) ^ a * kd,
+                                         0))
   }
 }
 
