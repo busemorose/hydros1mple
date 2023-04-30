659be852f62b52623a3ac15f0196d6c5761253e2
busemorose <guillaume.cinkus@gmail.com>
2023-04-30 14:16:01 +0200

    update score_year to score_seq: now include "month"

--- R/score_seq.R
+++ R/score_seq.R
@@ -3,6 +3,7 @@
 #' @param date A date vector.
 #' @param sim A numeric vector of simulated time series.
 #' @param obs A numeric vector of observed time series.
+#' @param freq A string specifying the frequence of analysis, either "hyear" for hydrological year or "month".
 #' @param month_hyear A numeric value corresponding to the first month of the hydrological year.
 #' @param lf_quantile A numeric value corresponding to the low flow threshold (lower than).
 #' @param hf_quantile A numeric value corresponding to the high flow threshold (higher than).
@@ -16,17 +17,18 @@
 #'
 #' @examples
 #' data(KarstMod_dataset)
-#' score_year(KarstMod_dataset$date, KarstMod_dataset$QobsS + runif(365, -2, +10), KarstMod_dataset$QobsS)
+#' score_seq(KarstMod_dataset$date, KarstMod_dataset$QobsS + runif(365, -2, +10), KarstMod_dataset$QobsS)
 
-score_year <- function(date, sim, obs, month_hyear = 9, lf_quantile = 0.1, hf_quantile = 0.9,
+score_seq <- function(date, sim, obs, freq = c("hyear", "month"),
+                       month_hyear = 9, lf_quantile = 0.1, hf_quantile = 0.9,
                        crit = c("NSE", "KGE", "rpearson", "rspearman", "KGE_m", "KGE_m2", "KGENP", "LME", "LCE"),
                        sf = c(1, 1, 1),
                        rel_heights = c(1, 0.9, 1.55)) {
 
   # Plot function
-  q_plot <- function(df) {
+  q_plot <- function(df, freq) {
 
-    ggplot(df, aes(hyear, score, fill = mean)) +
+    ggplot(df, aes(!!sym(freq), score, fill = mean)) +
       geom_bar(stat = "identity", width = 1, color = "black") +
       #geom_text(aes(label = ifelse(KGE < -1, round(KGE, 1), "")), y = 0.07, angle = 90) +
       coord_cartesian(ylim = c(ifelse(min(df$score, na.rm = TRUE) >= 0,
@@ -46,14 +48,17 @@ score_year <- function(date, sim, obs, month_hyear = 9, lf_quantile = 0.1, hf_qu
                                                  barwidth = 13,
                                                  barheight = 1.3,
                                                  direction = 'horizontal')) +
-      scale_x_continuous(breaks = seq(floor(min(df$hyear, na.rm = TRUE) / 10) * 10,
-                                      ceiling(max(df$hyear, na.rm = TRUE) / 10) * 10, 5)) +
+      {if (freq == "hyear") scale_x_continuous(breaks = seq(floor(min(df$hyear, na.rm = TRUE) / 10) * 10,
+                                                            ceiling(max(df$hyear, na.rm = TRUE) / 10) * 10, 5))} +
+      {if (freq == "month") scale_x_continuous(breaks = seq(1, 12, 1),
+                                               labels = seq(1, 12, 1))} +
       scale_y_continuous(breaks = c(0, 0.5, 1))
 
   }
 
   # Get output format
   crit <- match.arg(crit)
+  freq <- match.arg(freq)
 
   # Create dataframe
   model <- data.frame(date, sim, obs)
@@ -64,19 +69,19 @@ score_year <- function(date, sim, obs, month_hyear = 9, lf_quantile = 0.1, hf_qu
 
   x_all <- model |>
     dplyr::mutate(year = lubridate::year(date),
-           month = lubridate::month(date),
-           hyear = dplyr::case_when(month < month_hyear ~ year - 1,
-                             TRUE ~ year),
-           flow_type = dplyr::case_when(obs <= lf_t ~ "low",
-                                 obs >= hf_t ~ "high",
-                                 TRUE ~ "all"))
+                  month = lubridate::month(date),
+                  hyear = dplyr::case_when(month < month_hyear ~ year - 1,
+                                           TRUE ~ year),
+                  flow_type = dplyr::case_when(obs <= lf_t ~ "low",
+                                               obs >= hf_t ~ "high",
+                                               TRUE ~ "all"))
 
-  x_mean <- x_all |> dplyr::group_by(hyear) |> dplyr::summarise(mean = mean(obs, na.rm = TRUE))
+  x_mean <- x_all |> dplyr::group_by(!!sym(freq)) |> dplyr::summarise(mean = mean(obs, na.rm = TRUE))
   x_l <- x_all |> dplyr::filter(flow_type == "low")
   x_h <- x_all |> dplyr::filter(flow_type == "high")
 
   score <- dplyr::bind_rows(x_all |> dplyr::mutate(flow_type = "all"), x_l, x_h) |>
-    dplyr::group_by(hyear, flow_type) |>
+    dplyr::group_by(!!sym(freq), flow_type) |>
     dplyr::summarise(score = hydros1mple::score(sim, obs, crit = crit, sf = sf))
 
   # Generate plot
@@ -84,9 +89,9 @@ score_year <- function(date, sim, obs, month_hyear = 9, lf_quantile = 0.1, hf_qu
   ## hf_quantile
   x <- score |>
     dplyr::filter(flow_type == "high") |>
-    dplyr::left_join(x_mean, by = "hyear")
+    dplyr::left_join(x_mean, by = freq)
 
-  plot_h <- q_plot(x) +
+  plot_h <- q_plot(x, freq) +
     ylab(paste0(crit, "\n", ">Q", hf_quantile * 100)) +
     theme_bw(base_size = 16) +
     theme(axis.title.x = element_blank(),
@@ -98,9 +103,9 @@ score_year <- function(date, sim, obs, month_hyear = 9, lf_quantile = 0.1, hf_qu
   ## lf_quantile
   x <- score |>
     dplyr::filter(flow_type == "low") |>
-    dplyr::left_join(x_mean, by = "hyear")
+    dplyr::left_join(x_mean, by = freq)
 
-  plot_l <- q_plot(x) +
+  plot_l <- q_plot(x, freq) +
     ylab(paste0(crit, "\n", "<Q", lf_quantile * 100)) +
     theme_bw(base_size = 16) +
     theme(axis.title.x = element_blank(),
@@ -112,10 +117,11 @@ score_year <- function(date, sim, obs, month_hyear = 9, lf_quantile = 0.1, hf_qu
   ## all flow
   x <- score |>
     dplyr::filter(flow_type == "all") |>
-    dplyr::left_join(x_mean, by = "hyear")
+    dplyr::left_join(x_mean, by = freq)
 
-  plot_all <- q_plot(x) +
-    xlab(paste0("Hydrological year (", month.name[month_hyear], ")")) +
+  plot_all <- q_plot(x, freq) +
+    {if (freq == "hyear") xlab(paste0("Hydrological year (", month.name[month_hyear], ")"))} +
+    {if (freq == "month") xlab("Month")} +
     ylab(paste0(crit, "\n", "Q")) +
     theme_bw(base_size = 16) +
     theme(plot.margin = margin(-8, 8, 8, 8, "pt"),
--- README.md
+++ README.md
@@ -34,5 +34,5 @@ library(hydros1mple)
 - `km_properties()`: extract information from KarstMod *.properties* files
 - `cdf()`: cumulative distribution function of a variable
 - `insight()`: helping function for analysing sim/obs discharge from hydrological models
-- `score_year()`: generate plot to evaluate a model by hydrological year on different subregimes
+- `score_seq()`: generate plot to evaluate a model by hydrological year on different subregimes
 - `density_year()`: generate plot to show the density of criterion score by hydrological year
