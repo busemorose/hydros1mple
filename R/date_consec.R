4d6bc248519e7425b6d7d10594c56a3dc5fcdca2
busemorose <guillaume.cinkus@gmail.com>
2023-12-17 22:57:42 +0100

    Add `date_consec()`

---
+++ R/date_consec.R
@@ -0,0 +1,20 @@
+#' Identify consecutive dates.
+#'
+#' @param date A date vector.
+#'
+#' @return A dataframe.
+#' @export
+#'
+#' @examples
+#' date <- as.Date(c("2020-01-01", "2020-01-02", "2020-01-03", "2020-02-05", "2020-02-06", "2020-07-07"))
+#' km_import(path = path)
+
+date_consec <- function(date) {
+
+  data.table::data.table(date)[, consec := data.table::rleid(cumsum(c(FALSE, diff(date) > 1)))
+  ][, by = "consec",
+    .(min_date = min(date),
+      max_date = max(date))
+  ][, length := max_date - min_date + 1][]
+
+}
