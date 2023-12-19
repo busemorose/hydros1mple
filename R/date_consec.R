e607c902d4456e763782f022c27e6e9c84b8c36a
busemorose <guillaume.cinkus@gmail.com>
2023-12-19 10:05:13 +0100

    Add interval and data.table import

--- R/date_consec.R
+++ R/date_consec.R
@@ -1,15 +1,17 @@
 #' Identify consecutive dates.
 #'
 #' @param date A date vector.
+#' @param interval A numeric value corresponding to the interval in days, hour, minute or seconds.
 #'
 #' @return A dataframe.
+#' @import data.table
 #' @export
 #'
 #' @examples
 #' date <- as.Date(c("2020-01-01", "2020-01-02", "2020-01-03", "2020-02-05", "2020-02-06", "2020-07-07"))
 #' km_import(path = path)
 
-date_consec <- function(date) {
+date_consec <- function(date, interval) {
 
   data.table::data.table(date)[, consec := data.table::rleid(cumsum(c(FALSE, diff(date) > 1)))
   ][, by = "consec",
