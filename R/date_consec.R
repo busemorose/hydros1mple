1e707a35ba196198d93a898fbfe9a4fa7e42342e
busemorose <guillaume.cinkus@gmail.com>
2023-12-19 11:54:02 +0100

    Add unit

--- R/date_consec.R
+++ R/date_consec.R
@@ -11,12 +11,16 @@
 #' date <- as.Date(c("2020-01-01", "2020-01-02", "2020-01-03", "2020-02-05", "2020-02-06", "2020-07-07"))
 #' km_import(path = path)
 
-date_consec <- function(date, interval) {
+date_consec <- function(date, interval, unit) {
 
-  data.table::data.table(date)[, consec := data.table::rleid(cumsum(c(FALSE, diff(date) > interval)))
+  x <- data.table::data.table(date)[, consec := data.table::rleid(cumsum(c(FALSE, diff(date) > interval)))
   ][, by = "consec",
     .(min_date = min(date),
       max_date = max(date))
   ][, length := max_date - min_date + 1][]
 
+  units(x$length) <- unit
+  x$length <- format(round(x$length, 3), scientific = FALSE)
+
+  return(x)
 }
